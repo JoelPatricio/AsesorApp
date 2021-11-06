aeb4853b29435f25f7b2c34831d36cbbb38cbc2a
JoelPatricio <joelhp19@outlook.es>
2021-11-06 17:37:37 -0600

    Cracion de la conexion y el logout

---
+++ config/php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+try {
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbName = "asesorapp";
+  $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->exec('SET NAMES "utf8"');
+} catch (PDOexception $e) {
+  echo "ERROR: " . $e->getMessage();
+}
+?>
